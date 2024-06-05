9198e35a28ed41ef17ed47f23867edf40f8b4877
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-06-05 11:37:04 +0600

    use redux useDispatch hook

--- src/components/HooksCounter.jsx
+++ src/components/HooksCounter.jsx
@@ -1,29 +1,36 @@
-
 /* eslint-disable react/prop-types */
-import { useSelector } from "react-redux";
-// import { decrement, increment } from "./redux/counter/actionCreators";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/counter/actionCreators";
 
 export default function Counter() {
-  const count = useSelector((state) => state.value)
+	const count = useSelector((state) => state.value);
+	const dispatch = useDispatch();
+
+	const handleIncrement = () => {
+		dispatch(increment());
+	};
+
+	const handleDecrement = () => {
+		dispatch(decrement());
+	};
+
 	return (
 		<div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
 			<div className="text-2xl font-semibold">{count}</div>
-			{/* <div className="flex space-x-3">
+			<div className="flex space-x-3">
 				<button
 					className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-					onClick={increment}
+					onClick={handleIncrement}
 				>
 					Increment
 				</button>
 				<button
 					className="bg-red-400 text-white px-3 py-2 rounded shadow"
-					onClick={decrement}
+					onClick={handleDecrement}
 				>
 					Decrement
 				</button>
-			</div> */}
+			</div>
 		</div>
 	);
 }
-
-
